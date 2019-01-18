d08752da756b52c90c70b0fc10fc9c1a31d7c2cd
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2019-01-18 17:34:19 -0600

    update apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,31 @@
-var tableData = require("../data/friends");
+var friends = require("../data/friends");
+var arraySort = require("array-sort");
 
 //Routes
 module.exports = function(app) {
-
     //Display all possible friends
     app.get("/api/friends", function(req, res){
-        res.json(tableData);
+        res.json(friends);
     });
 
-
     app.post("api/friends", function(req, res){
-        var userData = req.body;
-        tableData.push(req.body);
-
-        res.json(tableData[tableData.length-1]);
-    })
+        //current user input
+        var user = req.body;
+        var totalDiffNumm
+        var totalDifferenceArray = [];
+        var results = 0;
+        for (var i = 0; i < friends.length; i++){
+            for (var j = 0; j < friends[i].scores.length; j++){
+                results +=  parseInt(user.scores[j]) - parseInt(friends[i].scores[j]);
+            }
+            totalDifferenceArray.push({
+                name: friends[i].name,
+                linkToPhoto: friends[i].linkToPhoto,
+                totalDiffNumm: results
+            });
+        }
+        arraySort(totalDifferenceArray, "totalDiffNum");
+        friends.push(user);
+        return res.json(totalDifferenceArray[0]);
+    });
 }
